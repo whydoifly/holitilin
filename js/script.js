6153b4ec5f3cffb3e9702d2e684547c8d3fae3e8
a.cheprasov <a.cheprasov@intermeda.ru>
2022-05-04 17:56:52 +0300

    Сделал десктоп

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let chars = 'Принять участие в исследовании '.split('');
-spin.innerHTML = chars.map((c, i) => {
-  return `<div class=char style="--a:${360/chars.length*i}deg">${c}</div>`
-}).join('');
+const text = document.querySelector(".circle__text p");
+text.innerHTML = text.innerText.split("").map((letter, i) =>
+`<span class="animated-text-span" style="transform: rotateZ(${((i - 1) * 11)}deg")>${letter}</span>`
+)
+.join("");
